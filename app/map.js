e5246badffec7ff1d34c50587e7a30238b784e30
Leo Kim <leobkkim@gmail.com>
2023-08-16 12:49:58 +0900

    feat: add map search box, search predictions, and location marker

--- app/map.js
+++ app/map.js
@@ -6,6 +6,9 @@ import MapView, { Marker } from "react-native-maps";
 export default function Map() {
   const [location, setLocation] = useState(null);
   const [initialRegion, setInitialRegion] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [searchedLocation, setSearchedLocation] = useState(null);
+  const [searchPredictions, setSearchPredictions] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
@@ -39,8 +42,63 @@ export default function Map() {
     getLocation();
   }, []);
 
+  const handleSearch = async () => {
+    if (!searchText) return;
+
+    try {
+      const mapAPIKey = process.env.EXPO_PUBLIC_MAP_API_KEY;
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${mapAPIKey}&input=${searchText}`,
+      );
+      const data = await response.json();
+
+      if (data.status === "OK" && data.predictions.length > 0) {
+        setSearchPredictions(data.predictions);
+        const placeId = data.predictions[0].place_id;
+        const placeDetailsResponse = await fetch(
+          `https://maps.googleapis.com/maps/api/place/details/json?key=${mapAPIKey}&place_id=${placeId}`,
+        );
+        const placeDetails = await placeDetailsResponse.json();
+
+        if (
+          placeDetails.status === "OK" &&
+          placeDetails.result.geometry &&
+          placeDetails.result.geometry.location
+        ) {
+          const location = placeDetails.result.geometry.location;
+          setSearchedLocation({
+            latitude: location.lat,
+            longitude: location.lng,
+          });
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching autocomplete data:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 3, margin: 10 }}
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+        onChange={handleSearch}
+        placeholder="Search"
+      />
+      {searchPredictions && (
+        <View style={styles.searchPredictions}>
+          <Text>
+            {searchPredictions.map((prediction, index) => (
+              <Text key={index}>
+                {prediction.structured_formatting.main_text}{" "}
+                {prediction.structured_formatting.secondary_text}
+                {"\n"}
+              </Text>
+            ))}
+          </Text>
+        </View>
+      )}
       {initialRegion && (
         <MapView
           style={styles.map}
@@ -51,6 +109,13 @@ export default function Map() {
         >
           {location && (
             <Marker coordinate={location.coords} title="My Location" />
+          )}
+          {searchedLocation && (
+            <Marker
+              coordinate={searchedLocation}
+              pinColor="green"
+              title={searchPredictions[0].structured_formatting.main_text}
+            />
           )}
         </MapView>
       )}
@@ -66,5 +131,15 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  searchPredictions: {
+    position: "absolute",
+    top: 50,
+    left: 10,
+    right: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    padding: 10,
+    zIndex: 1,
+    elevation: 2,
   },
 });
