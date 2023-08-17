2736acba97c1adaa29211c0cc36de99683e696f6
Leo Kim <leobkkim@gmail.com>
2023-08-17 23:27:07 +0900

    chore: configure metro bundler and dependencies for login and eas build

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
