f2b70fdf08af358e4c392c281b5419d29c064eb9
Leo Kim <leobkkim@gmail.com>
2023-08-19 10:23:43 +0900

    feat: add login UI; add firebase auth logic; add splash screen

--- app/index.js
+++ app/index.js
@@ -1,12 +1,34 @@
-import { Link } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import { useEffect } from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+
+import { auth } from "../firebaseConfig";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+    androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Link href="home/settings">Home</Link>
-      <StatusBar style="auto" />
+      <Image source={require("../assets/splash.png")} style={styles.logo} />
+      <TouchableOpacity style={styles.button} onPress={() => promptAsync()}>
+        <Text style={styles.buttonText}>Sign in with Google</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +36,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#000000",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 15,
+    fontWeight: "medium",
   },
 });
