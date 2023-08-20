61506036c3c2ace17647176ff468ca6ee82e8208
Leo Kim <leobkkim@gmail.com>
2023-08-20 10:31:27 +0900

    feat: add setting screen user profile UI; add sign out button

--- app/home/settings.js
+++ app/home/settings.js
@@ -1,9 +1,52 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { signOut } from "firebase/auth";
+import { useAtom } from "jotai";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
+
+import { auth } from "../../firebaseConfig";
+import { userAtom } from "../../userAtom";
 
 export default function Settings() {
+  const [user] = useAtom(userAtom);
+
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={styles.container}>
+      {user && (
+        <>
+          <Image source={{ uri: user.photoURL }} style={styles.userImage} />
+          <Text style={styles.greetingText}>Hello, {user.displayName} 👋</Text>
+          <Text>{user.email}</Text>
+        </>
+      )}
+      <Button
+        title="Sign out"
+        onPress={async () => {
+          await signOut(auth);
+          router.replace("/");
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 15,
+  },
+  greetingText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  userEmail: {
+    fontSize: 15,
+    fontWeight: "medium",
+  },
+});
