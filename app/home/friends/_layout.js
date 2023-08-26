3fd3a5d0ccbb0ced173c5cb9bea66f5c64134fb6
Leo Kim <leobkkim@gmail.com>
2023-08-26 21:37:27 +0900

    chore: change Friends stack navigator header to not be shown for UX

--- app/home/friends/_layout.js
+++ app/home/friends/_layout.js
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 export default () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Friends" }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Friends", headerShown: false }}
+      />
       <Stack.Screen name="friendList" options={{ title: "Friend List" }} />
       <Stack.Screen name="addFriend" options={{ title: "Add Friend" }} />
       <Stack.Screen
