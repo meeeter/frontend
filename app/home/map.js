4aadabe00c84d5fb9f094b5b300da260b6980b49
Leo Kim <leobkkim@gmail.com>
2023-08-27 17:17:23 +0900

    feat: add w3w mode button; add share via Kakaotalk button; configure Kakao SDK; update searchbox placeholder with current location info

--- app/home/map.js
+++ app/home/map.js
@@ -1,8 +1,19 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  Entypo,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 import * as Location from "expo-location";
 import { atom, useAtom } from "jotai";
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import KakaoShareLink from "react-native-kakao-share-link";
 import MapView, { Marker } from "react-native-maps";
 
 import { userAtom } from "../../userAtom";
@@ -17,9 +28,11 @@ export default function Map() {
   const [friendLocations, setFriendLocations] = useAtom(friendLocationsAtom);
   const [initialRegion, setInitialRegion] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [placeholderText, setPlaceholderText] = useState("Search");
   const [searchedLocation, setSearchedLocation] = useState(null);
   const [searchPredictions, setSearchPredictions] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [isW3wMode, setIsW3wMode] = useState(false);
 
   const socket = getSocket();
 
@@ -77,6 +90,16 @@ export default function Map() {
   }, []);
 
   useEffect(() => {
+    const updatePlaceholder = async () => {
+      setPlaceholderText(
+        isW3wMode ? location.w3w.words : location.address.name,
+      );
+    };
+
+    if (location) updatePlaceholder();
+  }, [location, isW3wMode]);
+
+  useEffect(() => {
     socket.on("friendLocationUpdate", (data) => {
       setFriendLocations((prevLocations) => {
         const updatedLocations = prevLocations.filter(
@@ -97,6 +120,10 @@ export default function Map() {
       socket.off("friendLocationUpdate");
     };
   }, []);
+
+  const handleSearchTextChange = (text) => {
+    setSearchText(text);
+  };
 
   const handleSearch = async () => {
     if (!searchText) return;
@@ -145,28 +172,138 @@ export default function Map() {
     ));
   };
 
+  const renderSearchPredictions = () => {
+    if (searchText && searchPredictions.length > 0) {
+      return (
+        <View style={styles.searchPredictions}>
+          {searchPredictions.map((prediction, index) => (
+            <Text key={index}>
+              {prediction.structured_formatting.main_text}{" "}
+              {prediction.structured_formatting.secondary_text}
+              {"\n"}
+            </Text>
+          ))}
+        </View>
+      );
+    }
+    return null;
+  };
+
+  const handleW3wButtonPress = () => {
+    setIsW3wMode((prevIsW3wMode) => !prevIsW3wMode);
+    // setPlaceholderText(isW3wMode ? location.w3w.words : location.address.name);
+  };
+
+  const handleShareButtonPress = async () => {
+    try {
+      const response = await KakaoShareLink.sendLocation({
+        address: `///${location.w3w.words}`,
+        addressTitle: "현재 내 위치 📍",
+        content: {
+          title: `현재 내 위치 📍 ///${location.w3w.words}`,
+          imageUrl: "https://i.postimg.cc/vZgSdbWC/meeeter.png",
+          link: {
+            mobileWebUrl: `http://map.kakao.com/link/search////${location.w3w.words}`,
+          },
+          description: "카카오맵에서 현재 내 위치를 확인하세요.",
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const w3wButtonColor = isW3wMode ? "#E11F26" : "white";
+  const w3wButtonTextColor = isW3wMode ? "white" : "#E11F26";
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      position: "relative",
+    },
+    searchContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      padding: 10,
+      backgroundColor: "#FFFFFF",
+      borderRadius: 20,
+      shadowColor: "#000",
+      shadowOpacity: 0.2,
+      shadowRadius: 2,
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      elevation: 2,
+      margin: 10,
+      zIndex: 2,
+    },
+    searchInput: {
+      flex: 1,
+      height: 50,
+      fontSize: 20,
+      paddingLeft: 10,
+    },
+    w3wButton: {
+      backgroundColor: w3wButtonColor,
+      borderRadius: 50,
+      width: 40,
+      height: 40,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    w3wButtonText: {
+      color: w3wButtonTextColor,
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    map: {
+      flex: 1,
+      zIndex: -1,
+    },
+    searchPredictions: {
+      position: "relative",
+      left: 10,
+      right: 10,
+      backgroundColor: "rgba(255, 255, 255, 0.7)",
+      padding: 10,
+      zIndex: 1,
+      elevation: 2,
+    },
+    shareButton: {
+      backgroundColor: "#FFFFFF",
+      borderRadius: 50,
+      width: 40,
+      height: 40,
+      alignItems: "center",
+      justifyContent: "center",
+      position: "absolute",
+      bottom: 20,
+      right: 20,
+      zIndex: 2,
+      elevation: 2,
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 3, margin: 10 }}
-        value={searchText}
-        onChangeText={(text) => setSearchText(text)}
-        onChange={handleSearch}
-        placeholder="Search"
-      />
-      {searchPredictions && (
-        <View style={styles.searchPredictions}>
-          <Text>
-            {searchPredictions.map((prediction, index) => (
-              <Text key={index}>
-                {prediction.structured_formatting.main_text}{" "}
-                {prediction.structured_formatting.secondary_text}
-                {"\n"}
-              </Text>
-            ))}
-          </Text>
-        </View>
-      )}
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          value={searchText}
+          onChangeText={handleSearchTextChange}
+          onChange={handleSearch}
+          placeholder={placeholderText}
+        />
+        <TouchableOpacity
+          onPress={handleShareButtonPress}
+          style={{ paddingRight: 10 }}
+        >
+          <SimpleLineIcons name="share-alt" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      {renderSearchPredictions()}
       {initialRegion && (
         <MapView
           style={styles.map}
@@ -175,10 +312,15 @@ export default function Map() {
           // showsUserLocation
           followsUserLocation
           userLocationPriority="balanced"
+          zIndex={-1}
         >
           {location && (
             <Marker coordinate={location.coords} title="My Location">
-              <MaterialCommunityIcons name="penguin" size={40} color="red" />
+              <MaterialCommunityIcons
+                name="penguin"
+                size={40}
+                color="#E11F26"
+              />
             </Marker>
           )}
           {renderFriendMarkers()}
@@ -188,32 +330,22 @@ export default function Map() {
               pinColor="green"
               title={searchPredictions[0].structured_formatting.main_text}
             >
-              <MaterialCommunityIcons name="penguin" size={40} color="green" />
+              <Entypo name="location-pin" size={40} color="green" />
             </Marker>
           )}
         </MapView>
       )}
+      <TouchableOpacity
+        style={{
+          ...styles.w3wButton,
+          position: "absolute",
+          bottom: 20,
+          right: 20,
+        }}
+        onPress={handleW3wButtonPress}
+      >
+        <Text style={styles.w3wButtonText}>///</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: "relative",
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-  },
-  searchPredictions: {
-    position: "absolute",
-    top: 50,
-    left: 10,
-    right: 10,
-    backgroundColor: "rgba(255, 255, 255, 0.7)",
-    padding: 10,
-    zIndex: 1,
-    elevation: 2,
-  },
-});
